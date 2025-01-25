48bce177ca487fa9a0d43b6d722d2cf455eb8c5f
astroscar09 <chavezoscar009@utexas.edu>
2025-01-25 17:30:17 -0600

    more tweaks to the functions

--- helper_functions.py
+++ helper_functions.py
@@ -24,11 +24,43 @@ def display_summary(OIII_ratios, SII_ratios, temp, den):
 
 
 def run_GetCritDensity(atom, K):
+
+    '''
+    Runs the Pyneb GetCritDensity function for a given atom and temperature
+    (Assumes a single temperature is passed)
+
+    Parameters:
+    atom: Pyneb Atom object
+        Atom object from Pyneb
+    K: float The temperature in Kelvin
+
+    Returns:
+    crit_density: list of float
+        List of critical densities for the given temperature
+
+    '''
     
     return atom.getCritDensity(K)
 
 
 def dist_getcritdensity(atom, K, return_type = 'array'):
+
+    '''
+    Function to get the critical densities for a range of temperatures 
+    this function can handle NaN values in the temperature array
+    it does so by appending the critical densities as NaN values
+
+    Parameters:
+    atom: Pyneb Atom object
+        Atom object from Pyneb
+    K: list of floats, temperatures in Kelvin
+    return_type: str, optional
+        Type of the return, 'array' or 'df'
+
+    Returns:
+    store_crit_density: array or DataFrame
+        Array or DataFrame of critical densities for the given temperatures
+    '''
 
     #making a two 2 array to hold the results of the critical densities
     #len(K) is how many temperatures we have
@@ -53,30 +85,65 @@ def dist_getcritdensity(atom, K, return_type = 'array'):
 
 def convert_int_relative_to_hbeta(int_ion, int_hbeta):
 
+    '''
+    Function to convert the intensity of an ion relative to Hbeta *100 as per PyNeb requirements
+
+    Parameters:
+    int_ion: float
+        Intensity of the ion
+    int_hbeta: float
+        Intesnity of Hbeta
+
+    Returns:
+    int_ratio: float
+        Intensity of the ion relative to Hbeta * 100
+    '''
+
     return (int_ion/int_hbeta) * 100
 
 def ionic_abundance(ion, int, tem, den, wave):
+
+    '''
+    Function that gets the ionic abundance for a given ion
+
+    Parameters:
+    ion: Pyneb Ion object
+        Ion object from Pyneb
+    int: float Intesnity of the ion relative to Hbeta * 100
+    tem: float Temperature in Kelvin
+    den: float Electron density in cm-3
+    wave: float Wavelength of the ion
+
+    Returns:
+    ion_abu: float
+        Ionic abundance of the ion
+
+    '''
     
     ion_abu = ion.getIonAbundance(int_ratio = int, tem = tem, den = den, wave = wave)
     
     return ion_abu
 
-def assign_temperature(temp, value):
+def replace_NaNs(arr, value):
+
+    '''
+    Function to assign a value to NaN values in an array
+
+    Parameters:
+    temp: list of float
+        array of values
+    value: float
+        Value to replace NaN values with
+
+    Returns:
+    temp_arr: list of float
+        array with NaN values replaced
+    '''
     
-    temp_arr = temp.copy() #added this copy to not overwrite the original input array
+    temp_arr = arr.copy() #added this copy to not overwrite the original input array
 
     for idx, temp in enumerate(temp_arr):
         if np.isnan(temp):
             temp_arr[idx] = value
     
     return temp_arr
-
-def assign_density(den, value):
-    
-    den_arr = den.copy() #added this copy to not overwrite the original input array
-    
-    for idx, den in enumerate(den_arr):
-        if np.isnan(den):
-            den_arr[idx] = value
-    
-    return den_arr
